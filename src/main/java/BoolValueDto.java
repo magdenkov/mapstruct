295c8284eb7fa1125add3e402048597a01737f74
denis.magdnekov <denis.magdnekov@bubbl.tech>
2017-06-05 17:37:03 +0300

    Initial commit

---
+++ src/main/java/BoolValueDto.java
@@ -0,0 +1,16 @@
+import com.google.protobuf.BoolValue;
+
+/**
+ * Created by Denis Magdenkov on 05.06.2017.
+ */
+public class BoolValueDto {
+    private BoolValue boolValue;
+
+    public BoolValue getBoolValue() {
+        return boolValue;
+    }
+
+    public void setBoolValue(final BoolValue boolValue) {
+        this.boolValue = boolValue;
+    }
+}
---
+++ src/main/java/BooleanDto.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Denis Magdenkov on 05.06.2017.
+ */
+public class BooleanDto {
+    private Boolean boolValue;
+
+    public Boolean getBoolValue() {
+        return boolValue;
+    }
+
+    public void setBoolValue(final Boolean boolValue) {
+        this.boolValue = boolValue;
+    }
+}
